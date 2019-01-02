8f76d0f993451038d8285e89dc0653f1a14de23c
Soufyan Bargach <soufyanb2@hotmail.com>
2019-01-02 09:26:50 +0100

    add action

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,10 @@
-/**
- * Description of the action goes here
- * @param  {String} params.name=value Description of the parameter goes here
- * @param  {Number} [params.age] Optional parameter
- */
-async function yourCustomAction(state, event, params) {
-  return state
-}
+const actions = {
 
-renderSelect: async (state, event) => {
-  await event.reply('#select', state);
-},
+  renderSelect: async (state, event) => {
+      await event.reply('#select', state);
+  },
 
-module.exports = { yourCustomAction, renderSelect }
+
+};
+module.exports = actions;
+
